997004011b16d672e21782fe293cf69c72171dc0
JohnnyDiBlasi <johnnyadiblasi@gmail.com>
2019-04-26 15:20:14 -0500

    Added new methods for array conversion and multiplication and Matrix Product

--- matrix.js
+++ matrix.js
@@ -20,4 +20,98 @@ class Matrix {
 			}
 		}
 	}
+
+	convertToMatrix(theArray) {
+		let m = new Matrix(theArray.length, 1);
+		for (let i = 0; i < theArray.length; i++) {
+			m.data[i][0] = theArray[i];
+		}
+		return m;
+	}
+
+	convertToArray() {
+		let theArray = [];
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				theArray.push(this.data[i][j]);
+			}
+		}
+		return theArray;
+	}
+
+	multiply(n) {
+		if (n instanceof Matrix) {
+			for (let i = 0; i < result.rows; i++) {
+				for (let j = 0; j < result.cols; j++) {
+					this.data[i][j] *= n.data[i][j];
+				}
+			}
+		}
+		else {
+			for (let i = 0; i < this.rows; i++) {
+				for (let j = 0; j < this.cols; j++) {
+					this.data[i][j] *= n;
+				}
+			}
+		}
+	}
+
+
+	//Matrix Product:
+	static matrixProduct(a, b) {
+		if (a.cols != b.rows) {
+			console.log('The Columns of Matrix A MUST MATCH the Rows of Matrix B');
+			return undefined;
+		}
+		let result = new Matrix(a.rows, b.cols);
+		for (let i = 0; i < result.rows; i++) {
+			for (let j = 0; j < result.cols; j++) {
+				let sum = 0;
+				for (let k = 0; k < a.cols; k++) {
+					sum += a.data[i][k] * b.data[k][j];
+				}
+				result.data[i][j] = sum;
+			}
+		}
+		return result;
+	}
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
